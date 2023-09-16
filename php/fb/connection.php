1aa54ff02b88e28eaf2fe848ba5d33931629122c
Faisal brahvii <114810964+faisalbrahviii@users.noreply.github.com>
2023-09-16 22:11:03 +0500

    Delete php/fb directory

--- php/fb/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $server = "mysql:host=localhost;dbname=faisal";
-    $user = "root";
-    $password ="";
-
-    $pdo = new PDO($server, $user, $password);
-?>
--- php/fb/forr.php
+++
@@ -1,8 +0,0 @@
-<?php
-$array = array("sami", "samad", "ali");
-
-for ($i=0;$i<count($array); $i++) {
-  echo $array[$i] . "<br>";
-};
-
-?>
